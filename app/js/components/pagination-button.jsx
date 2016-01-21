36de08f90d1e0f9a0d1747ea51771349ad3a4652
Kristen Mills <kristen@kristen-mills.com>
2016-01-20 18:41:15 -0800

    Pagination components

---
+++ app/js/components/pagination-button.jsx
@@ -0,0 +1,19 @@
+'use strict';
+
+import React from 'react';
+import { Link } from 'react-router';
+
+export default class PaginationButton extends React.Component {
+  constructor(path) {
+    super();
+    this.state = { path };
+  }
+
+  render() {
+    return (
+      <Link to={`/officers/edit`} query={{ page: this.props.eventKey }}>
+        {this.props.children}
+      </Link>
+    );
+  }
+}
