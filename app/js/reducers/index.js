4f792282050c5e0e22bb4833546d0d96990f6605
Kristen Mills <kristen@kristen-mills.com>
2016-01-21 18:52:41 -0800

    Added committees

--- app/js/reducers/index.js
+++ app/js/reducers/index.js
@@ -1,9 +1,12 @@
 'use strict';
+
 import { combineReducers } from 'redux';
 import auth from './auth';
 import officers from './officers';
+import committees from './committees';
 
 export default combineReducers({
   auth,
   officers,
+  committees,
 });
