28a4d821c50fb090a90f362d2e88a82769f14965
Dylan Vuz <vuzdylan@gmail.com>
2016-02-02 17:53:36 -0500

    here is a fix

--- app/js/components/form-modal.jsx
+++ app/js/components/form-modal.jsx
@@ -40,11 +40,12 @@ class FormModal extends React.Component {
   }
 
   submit(values) {
+    console.log("Values", values);
     const officer = Object.assign({}, values);
     officer.primaryOfficer = Boolean(officer.primaryOfficer);
 
     officer.startDate = moment.tz(officer.startDate, 'America/New_York').utc().toDate();
-    if (officer.endDate) {
+    if (officer.endDate && officer.endDate.valueOf() !== "") {
       officer.endDate = moment.tz(officer.endDate.valueOf(), 'America/New_York').utc().toDate();
     } else {
       officer.endDate = null;
